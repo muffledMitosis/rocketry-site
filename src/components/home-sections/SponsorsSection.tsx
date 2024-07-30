17d56fcf97c8cf74488fcc21e8eaf58ac34faa5c
Meth Munindradasa <methsndr@gmail.com>
2024-07-30 23:48:14 +1000

    Add proper images

--- src/components/home-sections/SponsorsSection.tsx
+++ src/components/home-sections/SponsorsSection.tsx
@@ -1,20 +1,46 @@
+import imageTop from '@/assets/images/sponsors/imageTop.jpg';
+import imageMiddle from '@/assets/images/sponsors/imageMiddle.jpg';
+import imageBottom from '@/assets/images/sponsors/imageBottom.jpg';
+
+import { fontAirstrike } from '@/lib/localfonts';
+
+import Image from 'next/image';
+
 export default function SponsorsSection() {
-	return (
-		<div className="flex flex-row w-screen h-screen border border-red-500">
-			<div className="border border-green-500 w-2/3 h-full">
-
-			</div>
-			<div className="border border-purple-500 w-1/3 h-full flex flex-col">
-				<div className="w-full h-1/3 border border-yellow-500">
-
-				</div>
-				<div className="w-full h-1/3 border border-yellow-500">
-
-				</div>
-				<div className="w-full h-1/3 border border-yellow-500">
-
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-row w-screen h-screen">
+      <div className="w-2/3 h-full">
+        <h1 className={fontAirstrike.className + ' text-4xl p-4'}>Sponsors</h1>
+      </div>
+      <div className="w-1/3 h-full flex flex-col">
+        <div className="relative w-full h-1/4">
+          <Image
+            src={imageTop}
+            alt="Picture of a team"
+            objectPosition="center"
+            objectFit="cover"
+            fill
+          />
+        </div>
+        <div className="relative w-full h-1/4">
+          <Image
+            src={imageMiddle}
+            alt="Picture of a team"
+            objectPosition="center"
+            objectFit="cover"
+            fill
+          />
+        </div>
+        <div className="relative w-full h-2/4">
+          <Image
+            src={imageBottom}
+            alt="Picture of a team"
+            objectPosition="center"
+            objectFit="cover"
+            fill
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/lib/teamInfo.ts
+++ src/lib/teamInfo.ts
@@ -1,21 +1,29 @@
-import aeroPic from '@/assets/images/teamBackgrounds/aerostructures.jpg'
+import aeroPic from '@/assets/images/teamBackgrounds/aerostructures.jpg';
+import avioPic from '@/assets/images/teamBackgrounds/avionics.jpg';
+import busiPic from '@/assets/images/teamBackgrounds/business.jpg';
+import paylPic from '@/assets/images/teamBackgrounds/payload.jpg';
+import propPic from '@/assets/images/teamBackgrounds/propulsion.jpg';
+import recPic from '@/assets/images/teamBackgrounds/recovery.jpg';
+import safePic from '@/assets/images/teamBackgrounds/safety.jpg';
+import simPic from '@/assets/images/teamBackgrounds/simulations.jpg';
+
 import { StaticImageData } from 'next/image';
 
 interface TeamInfo {
-	name: string,
-	quickInfo?: string,
-	backgroundImage?: StaticImageData,
+  name: string;
+  quickInfo?: string;
+  backgroundImage?: StaticImageData;
 }
 
 const teamInfo: TeamInfo[] = [
-  { name: 'Aerostructures', backgroundImage: aeroPic},
-  { name: 'Avionics' },
-  { name: 'Business' },
-  { name: 'Payload' },
-  { name: 'Propulsion' },
-  { name: 'Recovery' },
-  { name: 'Safety & Legal' },
-  { name: 'Simulations' },
+  { name: 'Aerostructures', backgroundImage: aeroPic },
+  { name: 'Avionics', backgroundImage: avioPic },
+  { name: 'Business', backgroundImage: busiPic },
+  { name: 'Payload', backgroundImage: paylPic },
+  { name: 'Propulsion', backgroundImage: propPic },
+  { name: 'Recovery', backgroundImage: recPic },
+  { name: 'Safety & Legal', backgroundImage: safePic },
+  { name: 'Simulations', backgroundImage: simPic },
 ];
 
 export default teamInfo;
