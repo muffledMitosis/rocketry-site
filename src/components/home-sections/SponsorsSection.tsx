ed9af4ecc6522156985f5f9056419d6ac5b75f15
Meth Munindradasa <methsndr@gmail.com>
2024-08-20 14:11:13 +1000

    Make it look okay on mobile

--- src/components/home-sections/SponsorsSection.tsx
+++ src/components/home-sections/SponsorsSection.tsx
@@ -8,12 +8,12 @@ import Image from 'next/image';
 
 export default function SponsorsSection() {
   return (
-    <div className="flex flex-row w-screen h-screen">
-      <div className="w-2/3 h-full">
+    <div className="flex flex-col md:flex-row w-screen h-screen">
+      <div className="w-full h-2/3 md:w-2/3 md:h-full">
         <h1 className={fontAirstrike.className + ' text-4xl p-4'}>Sponsors</h1>
       </div>
-      <div className="w-1/3 h-full flex flex-col">
-        <div className="relative w-full h-1/4">
+      <div className="h-1/3 w-full md:w-1/3 md:h-full flex flex-row md:flex-col">
+        <div className="relative h-full w-1/4 md:w-full md:h-1/4">
           <Image
             src={imageTop}
             alt="Picture of a team"
@@ -22,7 +22,7 @@ export default function SponsorsSection() {
             fill
           />
         </div>
-        <div className="relative w-full h-1/4">
+        <div className="relative h-full w-1/4 md:w-full md:h-1/4">
           <Image
             src={imageMiddle}
             alt="Picture of a team"
@@ -31,7 +31,7 @@ export default function SponsorsSection() {
             fill
           />
         </div>
-        <div className="relative w-full h-2/4">
+        <div className="relative h-full w-2/4 md:w-full md:h-2/4">
           <Image
             src={imageBottom}
             alt="Picture of a team"
