950cd5d7e3434b2a8cd38000fd1670fe56512f65
Meth Munindradasa <methsndr@gmail.com>
2024-11-25 13:06:28 +1100

    Add update script

---
+++ update.sh
@@ -0,0 +1,13 @@
+# Two ways of updating
+
+######
+
+# 1. push new code to server (rsync)
+# 2. Restart web service
+
+
+#####
+
+# 1. Web hook thingie attached to github
+# 2. Pull upon main branch push
+# 3. Restart process
