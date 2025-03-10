a16dde2701db4d36b8dcca6b8f6e25d57b76ac68
Meth Munindradasa <methsndr@gmail.com>
2025-03-10 22:25:30 +1100

    Add logo and fix spacing in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,23 @@
 import { headerLinks } from "@/data/header-content";
 import HeaderLink from "./header-link";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className="p-4 bg-rock-blue-dark">
-      <div className="flex flex-row">
+      <div className="flex flex-row justify-between">
+        {/* Logo */}
+        <div>
+          <Image 
+            src="/assets/images/LogoGoldLong.png"
+            alt="Logo"
+            width={200}
+            height={200}
+          />
+        </div>
+
         {/* Link List */}
-        <div className="flex flex-row space-x-4 text-white">
+        <div className="flex flex-row items-center space-x-4 text-white">
           {headerLinks.map((content) => (
             <HeaderLink key={content} linkContent={content} />
           ))}
