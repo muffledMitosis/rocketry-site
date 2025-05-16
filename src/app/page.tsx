6a73e1808cab7af054821caa6314dd7b924692bf
Meth Munindradasa <methsndr@gmail.com>
2025-05-16 20:18:16 +1000

    Remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import ParallaxBackground from "@/components/parallax-background"
 import AboutSection from "@/sections/About";
 import HeroSection from "@/sections/Hero";
 import Sponsors from "@/sections/Sponsors";
