d090a7160a1087076ea633dcdb625f835a10fc43
Meth Munindradasa <methsndr@gmail.com>
2025-06-22 03:00:58 +1000

    Remove sponsors and add join

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
+import JoinSection from "@/components/join";
 import AboutSection from "@/sections/About";
 import HeroSection from "@/sections/Hero";
-import Sponsors from "@/sections/Sponsors";
+// import Sponsors from "@/sections/Sponsors";
 
 export default function Home() {
   return (
     <>
       <HeroSection/>
       <AboutSection/>
-      <Sponsors/>
+      {/* <Sponsors/> */}
+      <JoinSection/>
     </>
   );
 }
