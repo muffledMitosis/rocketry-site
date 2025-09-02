af9248269176de210dd4cfdd7869c662a61cab09
Meth Munindradasa <methsndr@gmail.com>
2025-09-02 11:59:32 +1000

    Video cropped and slowed

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,7 +2,7 @@ import ParallaxVideoBackground from "@/components/parallax-video-background";
 
 const HeroSection = () => {
   return (
-    <ParallaxVideoBackground videoSrc="/assets/images/hero-video.mp4" startTime={50}>
+    <ParallaxVideoBackground videoSrc="/assets/images/hero-video.mp4" startTime={53}>
       <div className="flex items-center justify-center h-full">
       {/*<h1 className="text-4xl font-bold text-white">Parallax Section</h1>*/}
       </div>
